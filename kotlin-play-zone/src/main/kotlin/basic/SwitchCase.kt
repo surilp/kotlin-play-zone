7442e3f0ba21c2cdc4b17b8ea153d749385d9158
Suril Patel <surilsp@gmail.com>
2025-04-22 21:58:56 -0400

    creates basic folder

--- kotlin-play-zone/src/main/kotlin/basic/SwitchCase.kt
+++ kotlin-play-zone/src/main/kotlin/basic/SwitchCase.kt
@@ -1,4 +1,4 @@
-/*
+package basic/*
 Given the integer day denoting the day number, print on the screen which day of the week it is.
 Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
 Ensure only the 1st letter of the answer is capitalised.
