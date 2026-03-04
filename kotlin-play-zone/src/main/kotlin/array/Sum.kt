c0be9ac840d7010f8c80003e282b7f6e00fbee86
suril <surilsp@gmail.com>
2026-03-04 14:58:57 -0500

    Sum

---
+++ kotlin-play-zone/src/main/kotlin/array/Sum.kt
@@ -0,0 +1,19 @@
+package `kotlin-play-zone`.src.main.kotlin.array
+
+fun sum(arr: List<Int>, n: Int): Int {
+    var result = 0
+
+    arr.forEach {
+        result += it
+    }
+
+    return result
+}
+
+fun main() {
+    val input = listOf(1, 2, 3, 4, 5)
+    println("For input $input result is ${sum(input, input.size)}")
+
+    val input2 = listOf(1, 2, 1, 1, 5, 1)
+    println("For input $input result is ${sum(input2, input2.size)}")
+}
