85e40b99a115f81ee2d0a724f697a3be4dc38e48
suril <surilsp@gmail.com>
2026-03-04 11:18:26 -0500

    Armstrong fix

--- kotlin-play-zone/src/main/kotlin/math/Armstrong.kt
+++ kotlin-play-zone/src/main/kotlin/math/Armstrong.kt
@@ -1,14 +1,16 @@
 package `kotlin-play-zone`.src.main.kotlin.math
 
 import kotlin.math.pow
+import kotlin.math.log10
 
 fun isArmstrong(n: Int): Boolean {
     var result: Double = 0.0
     var num = n
+    val count = log10(n.toDouble()) + 1
 
     while(num > 0) {
         val currentDigit = num % 10
-        val powerThree = currentDigit.toDouble().pow(3)
+        val powerThree = currentDigit.toDouble().pow(count.toInt())
         result += powerThree
         num /= 10
     }
