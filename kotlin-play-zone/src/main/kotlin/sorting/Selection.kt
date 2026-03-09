841e54f1dcf25a07f23639206d1e1b49aecb7caa
suril <surilsp@gmail.com>
2026-03-09 18:34:01 -0400

    Selection Sort

---
+++ kotlin-play-zone/src/main/kotlin/sorting/Selection.kt
@@ -0,0 +1,29 @@
+package `kotlin-play-zone`.src.main.kotlin.sorting
+
+fun selectionSort(nums: IntArray): IntArray {
+    val n = nums.size
+
+    for (i in 0 until n) {
+        var minIndex = i
+        for (j in i + 1 until n) {
+            if (nums[minIndex] > nums[j]) {
+                minIndex = j
+            }
+        }
+        nums[i] = nums[minIndex].also { nums[minIndex] = nums[i] }
+    }
+
+    return nums
+}
+
+fun main() {
+    val input = listOf(
+        intArrayOf(7, 4, 1, 5, 3),
+        intArrayOf(5, 4, 4, 1, 1),
+        intArrayOf(3, 2, 3, 4, 5),
+    )
+
+    input.forEach {
+        println("For input ${it.contentToString()}, result is ${selectionSort(it).contentToString()}")
+    }
+}
