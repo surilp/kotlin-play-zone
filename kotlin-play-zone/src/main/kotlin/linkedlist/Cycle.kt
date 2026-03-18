1c18bbc6b84cf8162f4d1dd4a0efff431783aa0e
suril <surilsp@gmail.com>
2026-03-18 15:24:16 -0400

    Cycle

--- kotlin-play-zone/src/main/kotlin/linkedlist/Cycle.kt
+++ kotlin-play-zone/src/main/kotlin/linkedlist/Cycle.kt
@@ -24,6 +24,41 @@ fun hasCycle(head: Node<Int>?): Boolean {
     return false
 }
 
+fun meetingPoint(head: Node<Int>?): Node<Int>? {
+    if (head == null) {
+        return null
+    }
+
+    if (head == head.next) {
+        return head
+    }
+
+    var slow = head.next
+    var fast = head.next?.next
+
+    while (fast != null && fast.next != null) {
+        if (fast == slow) {
+            break
+        }
+
+        slow = slow?.next
+        fast = fast.next?.next
+    }
+
+    if (fast != slow) {
+        return null
+    }
+
+    slow = head
+
+    while (slow != fast) {
+        slow = slow?.next
+        fast = fast?.next
+    }
+
+    return slow
+}
+
 fun main() {
     // Example 1: 1 -> 2 -> 3 -> 4 -> 5 -> (back to 2), expected: true
     val n1 = Node(1); val n2 = Node(2); val n3 = Node(3)
@@ -31,6 +66,7 @@ fun main() {
     n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5
     n5.next = n2  // cycle to index 1
     println("Example 1: ${hasCycle(n1)}")  // true
+    println("Example 1: ${meetingPoint(n1)?.value}")  // true
 
     // Example 2: 1 -> 3 -> 7 -> 4, no cycle, expected: false
     val m1 = Node(1); val m2 = Node(3); val m3 = Node(7); val m4 = Node(4)
