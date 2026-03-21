5f169a5f3d343b9406b0fd3bf04c166907330009
suril <surilsp@gmail.com>
2026-03-21 17:30:55 -0400

    QueueWithStack

---
+++ kotlin-play-zone/stackqueue/QueueWithStack.kt
@@ -0,0 +1,60 @@
+package `kotlin-play-zone`.stackqueue
+
+class QueueWithStack {
+    var stack1 = ArrayDeque<Int>()
+    var stack2 = ArrayDeque<Int>()
+
+    fun push(x: Int) {
+        stack1.addLast(x)
+    }
+
+    fun pop(): Int {
+        if (isEmpty()) {
+            return -1
+        }
+
+        while (stack1.isNotEmpty()) {
+            stack2.addLast(stack1.removeLast())
+        }
+
+        val result = stack2.removeLast()
+
+        while (stack2.isNotEmpty()) {
+            stack1.addLast(stack2.removeLast())
+        }
+
+        return result
+    }
+
+    fun peek(): Int {
+        if (isEmpty()) {
+            return -1
+        }
+
+        while (stack1.isNotEmpty()) {
+            stack2.addLast(stack1.removeLast())
+        }
+
+        val result = stack2.last()
+
+        while (stack2.isNotEmpty()) {
+            stack1.addLast(stack2.removeLast())
+        }
+
+        return result
+    }
+
+    fun isEmpty(): Boolean {
+        return stack1.size == 0 && stack2.size == 0
+    }
+
+}
+
+fun main() {
+    val stack = QueueWithStack()
+    stack.push(4)
+    stack.push(8)
+    println(stack.pop())
+    println(stack.peek())
+    println(stack.isEmpty())
+}
